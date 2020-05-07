93674a8f19be78f6b9d27738fe45cf03d69e854c
FARMTROUGH <46006714+FARMTROUGH@users.noreply.github.com>
2020-05-07 08:24:26 -0500

    Update install_ubuntu.sh
    Updated to most current Dremio driver
    Changed to FARMTROUGH repot for dremio_odbc.py and the pngs
    Added the dremio_client[full] pip3 install
    Changed pip to pip3

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -32,13 +32,13 @@
 
 export REDASH_BASE_PATH=/opt/redash
 apt-get install -y alien unixodbc unixodbc-dev
-wget https://download.dremio.com/odbc-driver/1.3.19.1052/dremio-odbc-1.3.19.1052-1.x86_64.rpm
-alien -i --scripts dremio-odbc-1.3.19.1052-1.x86_64.rpm
-rm -f dremio-odbc-1.3.19.1052-1.x86_64.rpm
-pip install pyodbc pandas
-wget -O $REDASH_BASE_PATH/current/redash/query_runner/dremio_odbc.py "https://raw.githubusercontent.com/Dezota/DremioDSforRedash/master/dremio_odbc.py"
-wget -O $REDASH_BASE_PATH/current/client/dist/images/db-logos/dremio_odbc.png "https://raw.githubusercontent.com/Dezota/DremioDSforRedash/master/dremio_odbc.png"
-wget -O $REDASH_BASE_PATH/current/client/app/assets/images/db-logos/dremio_odbc.png "https://raw.githubusercontent.com/Dezota/DremioDSforRedash/master/dremio_odbc.png"
+wget https://download.dremio.com/odbc-driver/1.4.2.1003/dremio-odbc-1.4.2.1003-1.x86_64.rpm
+alien -i --scripts dremio-odbc-1.4.2.1003-1.x86_64.rpm
+rm -f dremio-odbc-1.4.2.1003-1.x86_64.rpm
+pip3 install pyodbc pandas dremio_client[full]
+wget -O $REDASH_BASE_PATH/current/redash/query_runner/dremio_odbc.py "https://raw.githubusercontent.com/FARMTROUGH/DremioDSforRedash/master/dremio_odbc.py"
+wget -O $REDASH_BASE_PATH/current/client/dist/images/db-logos/dremio_odbc.png "https://raw.githubusercontent.com/FARMTROUGH/DremioDSforRedash/master/dremio_odbc.png"
+wget -O $REDASH_BASE_PATH/current/client/app/assets/images/db-logos/dremio_odbc.png "https://raw.githubusercontent.com/FARMTROUGH/DremioDSforRedash/master/dremio_odbc.png"
 # Make sure this is the only entry for REDASH_ADDITIONAL_QUERY_RUNNERS.  If you have others then update env manually
 echo "export REDASH_ADDITIONAL_QUERY_RUNNERS=redash.query_runner.python,redash.query_runner.dremio_odbc" >> $REDASH_BASE_PATH/.env
 supervisorctl restart all
